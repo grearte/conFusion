8ce16a6f67010c510990750903d7f070c2b1cff6
grearte <gustavorearte@LDPUSLTP0019.attlocal.net>
2021-08-06 11:33:02 -0700

    ConFusion Ristorante final

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,102 @@
+/*
+
+INSTALL THESE 
+
+sudo npm install -g gulp-cli@2.0.1
+npm install gulp@3.9.1 --save-dev
+npm install gulp-sass@3.1.0  browser-sync@2.23.6 --save-dev
+
+npm install del@3.0.0 --save-dev
+npm install gulp-imagemin@4.1.0 --save-dev
+npm install gulp-uglify@3.0.0 gulp-usemin@0.3.29 gulp-rev@8.1.1 gulp-clean-css@3.9.3 gulp-flatmap@1.0.2 gulp-htmlmin@4.0.0 --save-dev
+
+*/
+
+
 'use strict';
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
-    'use strict';
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
- 
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-    });
-    
-    gulp.task('sass:watch', function () {
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
+  
+  gulp.task('sass:watch', function () {
     gulp.watch('./css/*.scss', ['sass']);
-    });
-    
-    gulp.task('browser-sync', function () {
-        var files = [
+  });
+  
+  gulp.task('browser-sync', function () {
+     var files = [
         './*.html',
         './css/*.css',
         './img/*.{png,jpg,gif}',
         './js/*.js'
-        ];
-    
-        browserSync.init(files, {
+     ];
+  
+     browserSync.init(files, {
         server: {
-            baseDir: "./"
+           baseDir: "./"
         }
-        });
-    
-    });
-    
-    // Default task
-    gulp.task('default', ['browser-sync'], function() {
-        gulp.start('sass:watch');
-    });
+     });
+  
+  });
+  
+// Default task -- gulp.start has been depricated in gulp 4.0 use gulp series instead
+gulp.task('default', ['browser-sync'], function() {
+    gulp.start('sass:watch');
+});
+
+/* It should be changed to the latest format
+  gulp.task('default', gulp.series ('sass:watch', gulp.parallel('browser-sync'),
+    function (done) { done(); }    
+));
+*/
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(gulp.dest('dist/img'));
+  });
+
+  gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+          .pipe(usemin({
+              css: [ rev() ],
+              html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+              js: [ uglify(), rev() ],
+              inlinejs: [ uglify() ],
+              inlinecss: [ cleanCss(), 'concat' ]
+          }))
+      }))
+      .pipe(gulp.dest('dist/'));
+  });
+  
+  // gulp.start has been depricated in gulp 4.0 use gulp.series, gulp.parallel instead
+  gulp.task('build',['clean'], function() {
+      gulp.start('copyfonts','imagemin','usemin');
+  });
